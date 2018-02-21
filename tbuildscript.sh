a3333958e678bf83ef1222fb6d5ac4fac93e333b
vgaws2018 <36676144+vgaws2018@users.noreply.github.com>
2018-02-21 12:20:20 +0000

    Update tbuildscript.sh

--- tbuildscript.sh
+++ tbuildscript.sh
@@ -10,7 +10,7 @@ wget http://www-eu.apache.org/dist/tomcat/tomcat-8/v8.5.28/bin/apache-tomcat-8.5
 #Installing tomcat
 tar -xvzf apache-tomcat-8.5.28.tar.gz
 cd apache-tomcat-8.5.28/bin/
-chmod 777 startup.sh shutdown.sh
+chmod 777 startup.sh shutdown.sh catalina.sh
 
 #Changing port of tomcat
 cd /opt/tomcat/apache-tomcat-8.5.28/conf
@@ -22,8 +22,8 @@ sed -i 's/8080/8088/g' server.xml
 # War deployement 
 cd /opt/tomcat/apache-tomcat-8.5.28/webapps/
 wget https://tomcat.apache.org/tomcat-8.0-doc/appdev/sample/sample.war 
-cd /opt/tomcat/apache-tomcat-8.5.28/bin/
+sh /opt/tomcat/apache-tomcat-8.5.28/bin/catalina.sh start
 # starting Tomcat services.
-./startup.sh
+
 
 
